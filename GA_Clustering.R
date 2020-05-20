9e12fd0e8d024622b9116e103533dd24be1b0b19
Pedro José Correia Duarte <40341487+pedrojcd@users.noreply.github.com>
2020-05-20 10:41:32 +0200

    Update GA_Clustering.R

--- GA_Clustering.R
+++ GA_Clustering.R
@@ -9,12 +9,17 @@ ga_clustering <- function(data, dim, n_clusters, pop_size, num_iterations, cross
   
 }
 
-scenarios_generation <- function(mu1, mu2, n_data_points = 200) {
+scenarios_generation <- function(){
+  
   # Scenario 1
-  x1 = mvrnorm(n_data_points/2, mu = mu1, Sigma = .1*diag(2))
-  x2 = mvrnorm(n_data_points/2, mu = mu2, Sigma = .1*diag(2))
-  y1 <- rep("Yes",100)
-  y2 <- rep("No",100)
+  mu1 = c(2,2)
+  mu2 = c(-2,2)
+  
+  n_data_points = 10
+  x1 = mvrnorm(n_data_points/2, mu = mu1, Sigma = .5*diag(2))
+  x2 = mvrnorm(n_data_points/2, mu = mu2, Sigma = .5*diag(2))
+  y1 <- rep("Yes",n_data_points/2)
+  y2 <- rep("No",n_data_points/2)
   
   y <- as.factor(c(y1, y2))
   X <- rbind(x1, x2)
@@ -24,45 +29,41 @@ scenarios_generation <- function(mu1, mu2, n_data_points = 200) {
   
   #plots
   cols = 3-ifelse(data_scenario1[,1]== "Yes",1, -1)
-  plot(data_scenario1[,-1], col = cols, main = "Scenario 1")
+  plot(data_scenario1[,-1], col = cols, main = "Scenario 1", ylim = c(0,4), xlim = c(-5,5))
   legend('topleft', c('Yes', 'No'), pch = 1, col = c(2, 4))
   
+  
   # Scenario 2
-  mean_matrix <- mvrnorm(10, mu = mu1, Sigma = diag(2))
-  mean_matrix_2 <- mvrnorm(10, mu = mu2, Sigma = diag(2))
-  data_yes <- c()
-  data_no <- c()
-  for (kk in 1:10) {
-    aux_yes <- mvrnorm(10, mu = mean_matrix[kk,], Sigma = .1*diag(2))
-    data_yes <- rbind(data_yes, aux_yes)
-    
-    aux_no <- mvrnorm(10, mu = mean_matrix_2[kk,], Sigma = .1*diag(2))
-    data_no <- rbind(data_no, aux_no)
-  }
+  mu1 = c(-3,3)
+  mu2 = c(0,1)
+  mu3 = c(3,3)
   
-  y_yes <- rep("Yes", n_data_points/2)
-  y_no <- rep("No", 100)
+  n_data_points = 300
+  x1 = runif(n_data_points/3, min = mu1-1, max = mu1+1)
+  x2 = runif(n_data_points/3, min = mu2-1, max = mu2+1)
+  x3 = runif(n_data_points/3, min = mu3-1, max = mu3+1)
+  y1 <- rep("1",n_data_points/3)
+  y2 <- rep("2",n_data_points/3)
+  y3 <- rep("3",n_data_points/3)
   
-  y <- as.factor(c(y_yes, y_no))
-  X <- rbind(data_yes, data_no)
+  y <- as.factor(c(y1, y2, y3))
+  X <- rbind(x1, x2, x3)
   
   data_scenario2 <- data.frame(y, X)
   colnames(data_scenario2) <- c("Y", "X1", "X2")
   
   #plots
-  cols = 3-ifelse(data_scenario2[,1]== "Yes",1, -1)
+  cols = ifelse(data_scenario2[,1]== '1',2,ifelse(data_scenario2[,1]== '2',3,4))
   plot(data_scenario2[,-1], col = cols, main = "Scenario 2")
-  legend('topleft', c('Yes', 'No'), pch = 1, col = c(2, 4))
+  legend('topleft', c('1', '2', '3'), pch = 1, col = c(2:4))
   
   return(list(data_scenario1, data_scenario2))
 }
 
 
 # Usage
+set.seed(2020)
 
-mu1 = c(0,0)
-mu2 = c(4,4)
-
-list_scenarios <- scenarios_generation(mu1, mu2)
+list_scenarios <- scenarios_generation()
 
                                        
