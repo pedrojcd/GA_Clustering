ccb781281f15bfff0e7e2757af339fe541f85f3f
Pedro José Correia Duarte <40341487+pedrojcd@users.noreply.github.com>
2020-06-10 12:12:47 +0200

    Suppression of unnecessary variables

--- GA_Clustering.R
+++ GA_Clustering.R
@@ -1,6 +1,10 @@
 setwd("~/GitHub/GA_Clustering")
 
 ##### PACKAGES #####
+
+#install.packages("")
+
+
 library(tidyverse)
 library(MASS)
 library(e1071)
@@ -8,8 +12,6 @@ library(ggplot2)
 library(EnvStats)
 library(pracma)
 library(plotrix)
-
-install.packages("plotrix")
 
 
 ##### FUNCTIONS #####
@@ -56,15 +58,15 @@ scenarios_generation <- function(){
   
   #plots
   cols = 3-ifelse(data_scenario1[,1]== "2",1, -1)
-  plot(data_scenario1[,-1], col = cols, main = "Scenario 1", ylim = c(-0.3,4.3), xlim = c(-4,4))
-  legend('topleft', c('Yes', 'No'), pch = 1, col = c(2, 4))
+  plot(data_scenario1[,-1], col = cols, main = "Data Set 1", ylim = c(-0.3,4.3), xlim = c(-4,4))
+  legend('topleft', c('class 1', 'class 2'), pch = 1, col = c(2, 4))
   
   
   # Data Set 2
-  mu1 = c(-2,2)
+  mu1 = c(-3,3)
   mu2 = c(0,1)
-  mu3 = c(2,2)
-  r = 1
+  mu3 = c(3,3)
+  r <- 1
   
   n_data_points = 300
   x1_x = runif(n_data_points/3, min = mu1[1]-1, max = mu1[1]+1)
@@ -109,12 +111,12 @@ scenarios_generation <- function(){
   
   #plots
   cols = ifelse(data_scenario2[,1]== '1',2,ifelse(data_scenario2[,1]== '2',3,4))
-  plot(data_scenario2[,-1], col = cols, main = "Scenario 2")
-  draw.circle(mu1[1],mu1[2],radius=1.1,nv=100,border="red",col="transparent",lty=1,density=NULL,
+  plot(data_scenario2[,-1], col = cols, main = "Data Set 2")
+  draw.circle(mu1[1],mu1[2],radius=1.3,nv=200,border="red",col="transparent",lty=1,density=NULL,
               angle=45,lwd=1)
-  draw.circle(mu2[1],mu2[2],radius=1.1,nv=100,border="green",col="transparent",lty=1,density=NULL,
+  draw.circle(mu2[1],mu2[2],radius=1.3,nv=200,border="green",col="transparent",lty=1,density=NULL,
               angle=45,lwd=1)
-  draw.circle(mu3[1],mu3[2],radius=1.1,nv=100,border="blue",col="transparent",lty=1,density=NULL,
+  draw.circle(mu3[1],mu3[2],radius=1.3,nv=200,border="blue",col="transparent",lty=1,density=NULL,
               angle=45,lwd=1)
   legend('bottomleft', c('class 1', 'class 2', 'class 3'), pch = 1, col = c(2:4))
   
@@ -167,7 +169,7 @@ scenarios_generation <- function(){
   colnames(data_scenario3) <- c("Y", "X1", "X2")
   
   #Plots
-  plot(X1 ~ X2, data=data_scenario3, type='n', ylim=c(-4, 4))
+  plot(X1 ~ X2, data=data_scenario3, type='n', ylim=c(-4, 4), main = "Data Set 3")
   for(i in 1:9){
     min = (i-1)*(n_data_points/9)+1
     max = i*(n_data_points/9)
@@ -214,9 +216,9 @@ delta<-function(a,b,c){
 }
 
 ## Algorithm Functions ##
-ga_clustering <- function(data, dim, n_clusters, pop_size, num_iterations, crossover_rate, mutation_rate, 
+ga_clustering <- function(data, n_clusters, pop_size, num_iterations, crossover_rate, mutation_rate, 
                           selection = "roulette", crossover = "single"){
-  
+  dim = ncol(data)
   sampleSize <- nrow(data)
   # Obtain lower and upper bounds to create the population
   maxValues <- max(data[1], na.rm = TRUE)
@@ -233,11 +235,11 @@ ga_clustering <- function(data, dim, n_clusters, pop_size, num_iterations, cross
   }
   
   # Define the fitness of the initial population
-  fitnessOfPop <- c(fitness(chromosome = population[1:(n_clusters*dim),], dim = dim, n_clusters = n_clusters, data = data, sampleSize))
+  fitnessOfPop <- c(fitness(population[1:(n_clusters*dim),], n_clusters, data))
   maxFitness = fitnessOfPop[1]
   best_hypothesis <- population[1:(n_clusters*dim),1]
   for(i in 2:pop_size){
-    fitnessOfPop <- cbind(fitnessOfPop,fitness(chromosome = population[1:(n_clusters*dim),i], dim = dim, n_clusters = n_clusters, data = data, sampleSize))
+    fitnessOfPop <- cbind(fitnessOfPop,fitness(population[1:(n_clusters*dim),i], n_clusters, data))
     if(maxFitness < fitnessOfPop[i]){
       maxFitness = fitnessOfPop[i]
       best_hypothesis <- population[1:(n_clusters*dim),i]
@@ -253,7 +255,7 @@ ga_clustering <- function(data, dim, n_clusters, pop_size, num_iterations, cross
   #Simulation
   for(i in 1:num_iterations){
     
-    print(1/maxFitness)
+    print(paste("best of iteration",i,"=",1/maxFitness))
     
     probability <- fitnessOfPop
     probability <- c(probability/sum(probability))
@@ -297,7 +299,7 @@ ga_clustering <- function(data, dim, n_clusters, pop_size, num_iterations, cross
     
     #Recompute fitness values
     for(i in 1:pop_size){
-      fitnessOfPop[i] <- fitness(chromosome = population[1:(n_clusters*dim),i], dim = dim, n_clusters = n_clusters, data = data, sampleSize = sampleSize)
+      fitnessOfPop[i] <- fitness(population[1:(n_clusters*dim),i], n_clusters, data)
       if(maxFitness < fitnessOfPop[i]){
         maxFitness = fitnessOfPop[i]
         best_hypothesis <- population[1:(n_clusters*dim),i]
@@ -305,19 +307,21 @@ ga_clustering <- function(data, dim, n_clusters, pop_size, num_iterations, cross
     }
   }
   
-  print(1/maxFitness)
   print(best_hypothesis)
-  
+  print(paste("Best of test",test,"=",1/maxFitness))
+              
   return(best_hypothesis)
   
 }
 
-fitness <- function(chromosome, dim, n_clusters, data, sampleSize){
-  return(1/clustering_metric(chromosome, dim, n_clusters, data, sampleSize))
+fitness <- function(chromosome, n_clusters, data){
+  return(1/clustering_metric(chromosome, n_clusters, data))
 }
 
-clustering_metric <- function(chromosome, dim, n_clusters, data, sampleSize){
+clustering_metric <- function(chromosome, n_clusters, data){
   sum <- 0
+  sampleSize <- nrow(data)
+  dim <- ncol(data)
   for(x in 1:sampleSize){
     min <- dist(rbind(chromosome[1:dim], data[x,1:dim]))
     for(j in 1:(n_clusters-1)){
@@ -336,8 +340,6 @@ clustering_metric <- function(chromosome, dim, n_clusters, data, sampleSize){
 ##### APPLICATION #####
 set.seed(2020)
 
-
-
 ### Artifical Data Sets Analysis ###
 list_scenarios <- scenarios_generation()
 
@@ -349,13 +351,12 @@ data = dataset1[2:3]
 for(test in 1:5){
   cluster_means <- kmeans(data,2, iter.max = 1000)
   print(cluster_means$centers)
-  print(clustering_metric(as.numeric(t(cluster_means$centers)),2,2,data,10))
+  print(clustering_metric(as.numeric(t(cluster_means$centers)),2,data))
 }
 
 # GA clustering
 for(test in 1:5){
-  ga_means <- ga_clustering(data, 2, 2, 10, 50, 0.8, 0.001)
-  print(clustering_metric(ga_means,2,2,data,10))
+  ga_means <- ga_clustering(data, 2, 10, 50, 0.8, 0.001)
 }
 
 
@@ -364,15 +365,14 @@ dataset2 = as.data.frame(list_scenarios[2])
 data = dataset2[2:3]
 # k-means clustering
 for(test in 1:5){
-  cluster_means <- kmeans(data,3, iter.max = 1000)
+  cluster_means <- kmeans(data, 3, iter.max = 1000)
   print(cluster_means$centers)
-  print(clustering_metric(as.numeric(t(cluster_means$centers)),2,2,data,300))
+  print(clustering_metric(as.numeric(t(cluster_means$centers)),3,data))
 }
 
 # GA clustering
 for(test in 1:5){
-  ga_means <- ga_clustering(data, 2, 2, 100, 50, 0.8, 0.001)
-  print(clustering_metric(ga_means,2,2,data,10))
+  ga_means <- ga_clustering(data, 3, 50, 50, 0.8, 0.001)
 }
 
 
@@ -383,13 +383,12 @@ data = dataset3[2:3]
 for(test in 1:5){
   cluster_means <- kmeans(data,9, iter.max = 1000)
   print(cluster_means$centers)
-  print(clustering_metric(as.numeric(t(cluster_means$centers)),2,2,data,450))
+  print(clustering_metric(as.numeric(t(cluster_means$centers)),9,data))
 }
 
 # GA clustering
 for(test in 1:5){
-  ga_means <- ga_clustering(data, 2, 9, 100, 50, 0.8, 0.001)
-  print(clustering_metric(ga_means,2,9,data,10))
+  ga_means <- ga_clustering(data, 9, 50, 50, 0.8, 0.001)
 }
 
 
@@ -400,48 +399,91 @@ data = dataset4[2:11]
 for(test in 1:5){
   cluster_means <- kmeans(data,2, iter.max = 1000)
   print(cluster_means$centers)
-  print(clustering_metric(as.numeric(t(cluster_means$centers)),10,2,data,300))
+  print(clustering_metric(as.numeric(t(cluster_means$centers)),2,data))
 }
 
 # GA clustering
 for(test in 1:5){
-  ga_means <- ga_clustering(data, 10, 2, 100, 50, 0.8, 0.001)
-  print(clustering_metric(ga_means,10,2,data,300))
+  ga_means <- ga_clustering(data, 2, 50, 50, 0.8, 0.001)
 }
+
+
+
+
 
 ### Real Life Data Sets Analysis ###
 iris_data <- read.csv("data/Iris.csv")
 lungCancer_data <- read.csv("data/lung-cancer.data.csv")
 transfusion_data <- read.csv("data/transfusion.data.csv")
-redWineQuality_data <- read.csv("data/winequality-red.csv", sep = ";")                                       
-whiteWineQuality_data <- read.csv("data/winequality-white.csv", sep = ";")
+redWineQuality_data <- read.csv("data/winequality-red.csv", sep = ";")       
+
+
+## Data Treatment : delete columns of strings
+lungCancer_data <- lungCancer_data[-c(5,39)]
+for(i in 1:55){
+  print(min(lungCancer_data[i]))
+}
+
 
 ## Iris Data Test
 data <- iris_data[2:5]
 
-# k-means test
+# k-means clustering
 for(test in 1:5){
   cluster_means <- kmeans(data,3, iter.max = 1000)
   print(cluster_means$centers)
-  print(clustering_metric(c(cluster_means$centers[1,1:4],cluster_means$centers[2,1:4],cluster_means$centers[3,1:4]),
-                4,3,data,150))
+  print(clustering_metric(as.numeric(t(cluster_means$centers)), 3, data))
 }
 
 # GA clustering
 for(test in 1:5){
-  ga_means <- ga_clustering(data, 4, 3, 100, 100, 0.8, 0.001)
-  print(clustering_metric(ga_means,4,3,data,150))
+  ga_means <- ga_clustering(data, 3, 50, 50, 0.8, 0.001)
 }
+
 
 ## Red Wine Data Test
 data <- redWineQuality_data[1:11]
+
+# k-means clustering
+for(test in 1:5){
+  cluster_means <- kmeans(data,6, iter.max = 1000)
+  print(cluster_means$centers)
+  print(clustering_metric(as.numeric(t(cluster_means$centers)), 6, data))
+}
+
+# GA clustering
+for(test in 1:5){
+  ga_means <- ga_clustering(data, 6, 50, 50, 0.8, 0.001)
+}
+
+
+
+## Transfusion Data Test
+data <- transfusion_data[1:4]
+
+# k-means clustering
+for(test in 1:5){
+  cluster_means <- kmeans(data,2, iter.max = 1000)
+  print(cluster_means$centers)
+  print(clustering_metric(as.numeric(t(cluster_means$centers)),2,data))
+}
+
+# GA clustering
+for(test in 1:5){
+  ga_means <- ga_clustering(data, 2, 50, 50, 0.8, 0.001)
+}
+
+## Lung Cancer Test
+data <- lungCancer_data[2:55]
+# k-means test
 for(test in 1:5){
   cluster_means <- kmeans(data,3, iter.max = 1000)
   print(cluster_means$centers)
-  metricVector <- c(cluster_means$centers[1:4])
+  print(clustering_metric(as.numeric(t(cluster_means$centers)),54,n_clusters = 3,data,31))
 }
 
-ga_means <- ga_clustering(data, 11, 6, 10, 100, 0.8, 0.001)
-
-
-# White Wine Data Test
+#GA clustering
+for(test in 1:5){
+  ga_means <- ga_clustering(data, 54, 3, 100, 100, 0.8, 0.001)
+  print(clustering_metric(ga_means,54,n_clusters = 3,data,31))
+}
